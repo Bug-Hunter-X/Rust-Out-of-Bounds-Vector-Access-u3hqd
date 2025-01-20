beb24c3207a998a31ae69e11fce2369275d701f5
BugHunter-X <askzenai@gmail.com>
2025-01-20 16:52:06 +0100

    Add bugSolution.rs file

---
+++ bugSolution.rs
@@ -0,0 +1,1 @@
+fn main() {    let mut numbers = vec![1, 2, 3, 4, 5];    let index = 10;    match numbers.get(index) {        Some(number) => println!("The number at index {} is {}", index, number),        None => println!("Index {} is out of bounds", index),    };}
